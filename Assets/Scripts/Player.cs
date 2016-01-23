48b7f129022e29c6bd7ca37d7a7007b56d19eb8b
Wasut <wasut_pao@hotmail.com>
2016-01-23 17:56:08 +0700

    Update

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,15 +10,28 @@ public class Player : MonoBehaviour {
 	public float forwardSpeed;
 	
 	private Rigidbody2D mainRigidbody2D;
+	private float maxWidth; //Max width of screen for player move
 
 	void Start()
 	{
 		mainRigidbody2D = GetComponent<Rigidbody2D> ();
 		mainRigidbody2D.isKinematic = true; //Player not fall when in PREGAME states
+
+		//Transforms position from screen space into world space.
+		Vector2 targetWidth = Camera.main.ScreenToWorldPoint (new Vector2 (Screen.width, Screen.height)); 
+		float playerWidth = GetComponent<Renderer>().bounds.extents.y; //Get brick width
+		maxWidth = targetWidth.y + playerWidth; //Calculate max width
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		//If Player go out off screen
+		if ((transform.position.y > maxWidth || transform.position.y < -maxWidth) && GameManager.instance.currentState == GameStates.INGAME) {
+			GameManager.instance.GameOver(); //Call game over
+		}
+
+
 		//When click or touch and in INGAME states
 		if ((Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space)) && GameManager.instance.currentState == GameStates.INGAME){
 			Jump();
@@ -27,8 +40,9 @@ public class Player : MonoBehaviour {
 		//When click or touch and in PREGAME states
 		if ((Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space)) && GameManager.instance.currentState == GameStates.PREGAME){
 			mainRigidbody2D.isKinematic = false; //Player can fall
-			GameManager.instance.StartGame();
+			GameManager.instance.StartGame(); //Start game
 		}
+
 	}
 	
 	void Jump(){
